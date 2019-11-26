3dc9bc526331738250f13ae8b0148f81d586f577
renato.t.tamaki <renato.t.tamaki@CPX-1076GIIXVZ0.dir.svc.accenture.com>
2019-11-26 15:07:23 -0200

    Criação do properties de test

--- src/test/java/com/tamaki/twitter/tweet/test/TwitterTweetTest.java
+++ src/test/java/com/tamaki/twitter/tweet/test/TwitterTweetTest.java
@@ -11,8 +11,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 //import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,7 +20,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.tamaki.twitter.App;
 
-@PropertySource("classpath:application-infra.properties")
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = App.class)
 @WebAppConfiguration
